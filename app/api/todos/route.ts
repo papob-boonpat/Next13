2d31ed1488bd2114e012b86b5945a463dfc91141
boonpat.papob <boonpat.papob@gmail.com>
2023-07-04 23:26:00 +0700

    Chapter 8
    - delete method
    - get mothod

---
+++ app/api/todos/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos";
+const API_KEY: string = process.env.DATA_API_KEY as string;
+
+export async function GET() {
+  const res = await fetch(DATA_SOURCE_URL);
+
+  const todos: Todo[] = await res.json();
+
+  return NextResponse.json(todos);
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  console.log(params);
+
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
+
+  if (!id) return NextResponse.json({ message: "Todo id required" });
+
+  await fetch(`${DATA_SOURCE_URL}/${id}`, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json", "API-Key": API_KEY },
+  });
+
+  return NextResponse.json({ message: `Todo ${id}  deleted` });
+}
