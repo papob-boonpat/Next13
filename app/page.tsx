73e39f13d8ec46d80b7c8982438e4fae0c31f5fb
papob.boonpat <boonpat.papob@gmail.com>
2023-07-19 23:51:39 +0700

    Add new revalidation

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Posts from "./components/Posts";
 
+export const revalidate = 10;
+
 export default function Home() {
   return (
     <main className="px-6 mx-auto">
